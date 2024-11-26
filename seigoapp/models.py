814aafe3dd430329a9ce8a27c164fafa0819288f
Seigo <tdn2430115@stu.o-hara.ac.jp>
2024-11-26 14:05:50 +0900

    'Commit'

--- seigoapp/models.py
+++ seigoapp/models.py
@@ -1,14 +1,7 @@
 from django.db import models
 
 class SeigoPost(models.Model):
-    stu_num = models.CharField(verbose_name= "学生番号" , max_length=100)
-    name = models.CharField(verbose_name= "名前" , max_length=100)
-    subject = models.CharField(verbose_name= "科目" , max_length=100)
-    score = models.CharField(verbose_name= "得点" , max_length=100)
-    datetime = models.DateField(verbose_name= "投稿日時" , auto_now_add=True )
-
-    def __str__(self):
-        return self.stu_num
+    pass
 
 class Student(models.Model):
 
